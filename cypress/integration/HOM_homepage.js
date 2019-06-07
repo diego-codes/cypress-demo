a66854a4370987f28376d78e970ae2e903b13360
Diego Hernandez <diego.a.hernandez123@gmail.com>
2019-06-06 21:55:25 -0500

    initial commit

---
+++ cypress/integration/HOM_homepage.js
@@ -0,0 +1,39 @@
+/// <reference types="Cypress" />
+
+const scrollToEndOfDoc = doc => {
+  cy.scrollTo(0, doc.body.clientHeight)
+}
+
+describe('Homepage', () => {
+  beforeEach(() => {
+    cy.server()
+    cy.route('https://api.flickr.com/services/rest/*').as('flickr')
+    cy.visit('http://diego-codes.github.io/ascension-nasa-flickr/')
+    cy.wait('@flickr')
+  })
+
+  it('loads more images when user scrolls to the bottom of the page', () => {
+    cy.document().then(scrollToEndOfDoc)
+    cy.wait('@flickr')
+    cy.document().then(scrollToEndOfDoc)
+    cy.wait('@flickr')
+  })
+
+  it('clicks through to a single image', () => {
+    cy.get('.thumbnail').first().click()
+    cy.hash().should('include', 'photo/')
+  })
+
+  it('searches for images when user types in value', () => {
+    cy.get('#search_input').type('Colombia{enter}')
+    cy.hash().should('include', 'q=Colombia')
+    cy.wait('@flickr')
+    cy.get('.thumbnail').first().click()
+    cy.wait('@flickr')
+    cy.get('.photo').contains('Colombia')
+  })
+
+  it('sorts images', () => {
+    cy.get('.sort__input').select('title')
+  })
+})
